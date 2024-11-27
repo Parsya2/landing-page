bdc4073733568a8d338f58108907d5c1e0f98014
amir <amir@gmail.com>
2024-11-27 18:05:24 +0330

    sec-2 added

--- src/components/sec.js
+++ src/components/sec.js
@@ -9,25 +9,86 @@ export const Sec = () => {
           <ul className="text-white">
             <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
               <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
-              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+              <p className="font-normal text-sm m-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
+                quam arcu nunc, orci eu bibendum.{" "}
+              </p>
             </li>
             <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
               <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
-              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+              <p className="font-normal text-sm m-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
+                quam arcu nunc, orci eu bibendum.{" "}
+              </p>
             </li>
             <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
               <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
-              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+              <p className="font-normal text-sm m-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
+                quam arcu nunc, orci eu bibendum.{" "}
+              </p>
             </li>
             <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
               <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
-              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+              <p className="font-normal text-sm m-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
+                quam arcu nunc, orci eu bibendum.{" "}
+              </p>
             </li>
             <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
               <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
-              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+              <p className="font-normal text-sm m-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
+                quam arcu nunc, orci eu bibendum.{" "}
+              </p>
             </li>
           </ul>
+        </div>
+        <div className="w-full text-white flex flex-col sm:flex-row">
+          <div className="my-5 border-l-2 pl-3">
+            <h1 className="font-normal text-2xl">Lorem.</h1>
+            <p className="font-normal text-sm">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+              itaque, magnam quos nemo soluta alias nostrum minus ipsam placeat
+              quia magni quo nobis, optio suscipit provident beatae natus
+              voluptas vitae eligendi aliquid impedit. Maxime, molestiae
+              reprehenderit, nisi delectus inventore iure veritatis accusamus
+              sequi itaque harum cum neque placeat. Repudiandae, odio!
+            </p>
+          </div>
+          <div className="my-5 border-l-2 pl-3">
+            <h1 className="font-normal text-2xl">Lorem.</h1>
+            <p className="font-normal text-sm">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+              itaque, magnam quos nemo soluta alias nostrum minus ipsam placeat
+              quia magni quo nobis, optio suscipit provident beatae natus
+              voluptas vitae eligendi aliquid impedit. Maxime, molestiae
+              reprehenderit, nisi delectus inventore iure veritatis accusamus
+              sequi itaque harum cum neque placeat. Repudiandae, odio!
+            </p>
+          </div>
+          <div className="my-5 border-l-2 pl-3">
+            <h1 className="font-normal text-2xl">Lorem.</h1>
+            <p className="font-normal text-sm">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+              itaque, magnam quos nemo soluta alias nostrum minus ipsam placeat
+              quia magni quo nobis, optio suscipit provident beatae natus
+              voluptas vitae eligendi aliquid impedit. Maxime, molestiae
+              reprehenderit, nisi delectus inventore iure veritatis accusamus
+              sequi itaque harum cum neque placeat. Repudiandae, odio!
+            </p>
+          </div>
+          <div className="my-5 border-l-2 pl-3">
+            <h1 className="font-normal text-2xl">Lorem.</h1>
+            <p className="font-normal text-sm">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore
+              itaque, magnam quos nemo soluta alias nostrum minus ipsam placeat
+              quia magni quo nobis, optio suscipit provident beatae natus
+              voluptas vitae eligendi aliquid impedit. Maxime, molestiae
+              reprehenderit, nisi delectus inventore iure veritatis accusamus
+              sequi itaque harum cum neque placeat. Repudiandae, odio!
+            </p>
+          </div>
         </div>
       </section>
     </>
