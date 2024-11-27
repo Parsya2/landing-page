7c0c5fe56d8d5c258bb97e2e761648c979a66e86
amir <amir@gmail.com>
2024-11-27 17:53:51 +0330

    sec-1 added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Nav from "./components/nav";
 import Header from "./components/header";
+import Sec from "./components/sec";
+
 const App = () => {
   return (
     <>
       <Nav />
       <Header/>
+      <Sec/>
     </>
   );
 };
---
+++ src/components/sec.js
@@ -0,0 +1,37 @@
+export const Sec = () => {
+  return (
+    <>
+      <section className="bg-green-800 border-t-2 p-4">
+        <div>
+          <h1 className="text-white font-normal text-3xl md:text-6xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <ul className="text-white">
+            <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
+              <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
+              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+            </li>
+            <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
+              <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
+              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+            </li>
+            <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
+              <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
+              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+            </li>
+            <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
+              <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
+              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+            </li>
+            <li className="border-y-2 my-5 hover:bg-green-200 hover:text-green-800">
+              <h1 className="font-normal text-2xl m-2">Lorem, ipsum.</h1>
+              <p className="font-normal text-sm m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam quam arcu nunc, orci eu bibendum. </p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Sec;
